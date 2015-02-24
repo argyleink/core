3a62769f931d2e1cd4da31c84e687b2c65b803d5
Jesse Weed <jesse@jesseweed.com>
2015-02-23 22:26:53 -0800

    setup Gulpfile

---
+++ Gulpfile.js
@@ -0,0 +1,35 @@
+var app = {},
+    path = require('path');
+
+// require(path.resolve('./config/_settings'))(app);
+// require(path.resolve('./config/environment/development'))(app);
+
+var gulp        = require('gulp'),
+    nodemon     = require('gulp-nodemon');
+
+
+// DEFAULT TASK, HANDLES ALL BASIC SERVER STUFF
+gulp.task('default', ['nodemon'], function () {
+});
+
+
+
+// NODEMON
+gulp.task('nodemon', function (cb) {
+  var called = false;
+  return nodemon({
+    script: 'server.js',
+    ext: 'js, hbs, less',
+    ignore: ['README.md', 'node_modules/**', '.DS_Store'],
+    'execMap': {
+      'js': 'iojs'
+    }
+  }).on('start', function () {
+    if (!called) {
+      called = true;
+      cb();
+    }
+  })
+  .on('restart', function () {
+  });
+});
