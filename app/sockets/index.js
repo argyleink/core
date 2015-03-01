ec744d3628b8d983515cebde0ee4aca7d5b459b0
Jesse Weed <jesse@jesseweed.com>
2015-02-28 17:00:11 -0800

    move info log to server.js

--- app/sockets/index.js
+++ app/sockets/index.js
@@ -3,7 +3,6 @@ module.exports = function (app) {
   const io = require('socket.io')(app.config.socket.port);
 
   io.on('connection', function(socket){
-    console.log('YAY! SOCKETS ARE WORKING!!!');
   });
 
 };
