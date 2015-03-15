502f14dba9f62875ede39cbadae486fc840dccf3
Jesse Weed <jesse@jesseweed.com>
2015-03-14 17:17:58 -0700

    fix socket.io issue and update to run on same port as pog

--- app/sockets/index.js
+++ app/sockets/index.js
@@ -1,8 +1,18 @@
-module.exports = function (app) {
+'use strict';
 
-  const io = require('socket.io')(app.config.socket.port);
+module.exports = function (app, session) {
 
-  io.on('connection', function(socket){
+  const io = require('socket.io')(app.server),
+        pog = app.pog,
+        colors = require('colors');
+
+  // ANNOUNCE THAT SOCKETS ARE ENABLED
+  app.log('INFO: '.blue + 'enabling ' + 'socket.io'.yellow + ' server');
+
+  // ON CONNECTION
+  io.on('connection', function (socket){
+
   });
+
 
 };
