53239511d6159d80eefd4ecc0c7a1f7101e2e4f1
Jesse Weed <jesse@jesseweed.com>
2015-03-15 19:39:24 -0700

    add custom route example in routes.js

--- app/routes.js
+++ app/routes.js
@@ -5,4 +5,19 @@
 
 module.exports = function(app) {
 
+  // EXAMPLE ROUTE USING KOA ROUTE: https://www.npmjs.com/package/koa-route
+  // UNCOMMENT CODE BELOW TO LOAD THE ROUT http://localhost:1982/blog/id
+
+  // const _ = require('koa-route');
+  //
+  // app.use(_.get('/blog/:id', function *(id) {
+  //
+  //   return yield this.render('blog', {
+  //     title : app.name,
+  //     site: app,
+  //     id: id
+  //   });
+  //
+  // }));
+
 };
